b9bf93ad8467808964d2bd22c89065d7c71b89bd
fgchng <ching_ferelyn@yahoo.com>
2024-10-14 23:01:11 +0800

    AJAX committed to NMap

--- frontend/js/sidebar.js
+++ frontend/js/sidebar.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
 
-    fetch('sidebar.html')
+    fetch('/frontend/sidebar.html')
         .then(response => {
             if (!response.ok) {
                 throw new Error('Failed to load sidebar: ' + response.statusText);
