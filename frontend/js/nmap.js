718d09d861f4674a75b4b6c637c1cd1260c64ba6
rianna <rianna_uy@dlsu.edu.ph>
2024-11-02 17:45:00 +0800

    Sidebar and nmap improvements

--- frontend/js/nmap.js
+++ frontend/js/nmap.js
@@ -1,6 +1,25 @@
+function getQueryParam(param) {
+    const urlParams = new URLSearchParams(window.location.search);
+    return urlParams.get(param);
+}
+
+
 document.getElementById('startScanBtn').addEventListener('click', function() {
     var ipAddress = document.getElementById('ipAddress').value;
     var subnetMask = document.getElementById('subnetMask').value;
 
     window.location.href = `nmap-network.html?ip=${ipAddress}&subnet=${subnetMask}`;
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Check if the "scan" query parameter is present in the URL
+    const scanType = getQueryParam("scan");
+
+
+        // Optional: Customize the modal title or content based on scan type
+        if (scanType === "network") {
+            $('#scanNetworkModal').modal('show'); // Show the Network Scan modal
+        } else if (scanType === "device") {
+            $('#scanDeviceModal').modal('show'); // Show the Device Scan modal
+        }
 });
