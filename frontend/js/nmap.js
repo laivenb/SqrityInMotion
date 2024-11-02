97f03bbaad2a67150dc1d097e9d4f8384ffa4ddc
fgchng <ching_ferelyn@yahoo.com>
2024-11-02 16:04:02 +0800

    Commit

--- frontend/js/nmap.js
+++ frontend/js/nmap.js
@@ -1,6 +1,16 @@
 document.getElementById('startScanBtn').addEventListener('click', function() {
-    var ipAddress = document.getElementById('ipAddress').value;
-    var subnetMask = document.getElementById('subnetMask').value;
+    // Check if the user is logged in by verifying the session storage
+    const currentUser = sessionStorage.getItem("username");
 
-    window.location.href = `nmap-network.html?ip=${ipAddress}&subnet=${subnetMask}`;
+    if (!currentUser) {
+        // Redirect to login if no user is logged in
+        window.location.href = "login.html";
+    } else {
+        // Retrieve IP and subnet from user input fields
+        var ipAddress = document.getElementById('ipAddress').value;
+        var subnetMask = document.getElementById('subnetMask').value;
+
+        // Redirect to the scan page with IP and subnet as query parameters
+        window.location.href = `nmap-network.html?ip=${ipAddress}&subnet=${subnetMask}`;
+    }
 });
